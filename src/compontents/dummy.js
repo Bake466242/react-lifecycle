be423158154dd8984037dfbfb5e23e2af1ee79ea
Rebecca Bakels <rebeccabakels@gmail.com>
2021-02-04 11:46:04 -0500

    simple click counter and lifecycle demo

---
+++ src/compontents/dummy.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class Dummy extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 0
+        }
+    }
+    componentDidMount() {
+        console.log('MOUNTED')
+    }
+    componentDidUpdate() {
+        console.log('Updated', this.state.counter)
+    }
+
+    render() {
+        const { counter } = this.state
+        return (
+        <>
+        <h1>Hello React Peeps</h1>
+        <button onClick={() => {
+            this.setState({ counter: counter+1})
+        }}>
+            Click Me
+            </button>
+        <h2>{counter}</h2>
+        </>
+        )
+    }
+}
+
+
+
+export default Dummy
